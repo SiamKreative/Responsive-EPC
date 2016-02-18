b90deff97886b3ddb6f76a3096688c2e51b86fde
Julien Vernet <julien@vernet.me>
2016-02-18 16:48:39 +0700

    Remove jQuery Dependency. Closes issue #2.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-jQuery(document).ready(function ($) {
+var graphs = document.getElementsByClassName('graph'),
+	l = graphs.length,
+	i, cur;
 
-	$('.graph').each(function (index, el) {
-
-		var selected = $(this).attr('data-selected');
-		$(this).find('.bar:eq(' + selected + ')').addClass('bar-selected');
-
-	});
-
-});
+for (i = 0; i < l; i++) {
+	cur = graphs[i];
+	var index = cur.getAttribute('data-selected');
+	var barSelected = cur.getElementsByClassName('bar' + index);
+	barSelected[0].className += ' bar-selected';
+}
