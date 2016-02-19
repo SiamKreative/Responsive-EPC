250761db0fb4c8f9ea6ed4a1aa9c87ed81d05fa6
Julien Vernet <julien@vernet.me>
2016-02-19 12:26:00 +0700

    Prefix everything. Closes #1

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var graphs = document.getElementsByClassName('graph'),
+var graphs = document.getElementsByClassName('epc_graph'),
 	l = graphs.length,
 	i, cur;
 
 for (i = 0; i < l; i++) {
 	cur = graphs[i];
-	var index = cur.getAttribute('data-selected');
-	var barSelected = cur.getElementsByClassName('bar' + index);
-	barSelected[0].className += ' bar-selected';
+	var index = cur.getAttribute('data-epcSelected');
+	var barSelected = cur.getElementsByClassName('epc_bar' + index);
+	barSelected[0].className += ' epc_bar_selected';
 }
